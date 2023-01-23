845ec60e9e9199adf6404cf349ac0aee9fe73093
HPA Macbook Pro <skcmhm@gmail.com>
2023-01-23 14:42:29 -0800

    Create hover algo Shouldve commited to git more often but this one version is basically done. Added in the hover algorithm which allows for users to paint by hovering over.

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ const button = document.querySelector('button')
 
 
 button.addEventListener('click', () => {
-    createGrid();
+    let windowSize = prompt("How Many Squares Do You Want Per Side", 32)
+    if (windowSize < 101){
+        createGrid(windowSize);
+    }
+    else {
+        alert("Try inputting a number less than 100!")
+    }
 })
 
 
-function createGrid() {
-    for (let i = 0; i < 16; i++){
+function createGrid(numberOfSquares) {
+    container.replaceChildren();
+
+    for (let i = 0; i < numberOfSquares; i++){
         const columns = document.createElement('div')
         columns.classList.add('columns')
         container.appendChild(columns)
 
-        for (let i = 0; i < 16; i++){
+        //Create grid squares by creating rows with in the columns
+        for (let j = 0; j < numberOfSquares; j++){
             const rows = document.createElement('div')
-            rows.classList.add('rows')
+            rows.classList.add('grid')
+            rows.addEventListener("mouseover", () => {
+                rows.classList.add('hover')
+            })
             columns.appendChild(rows)
         }
         
     }
 }
+
