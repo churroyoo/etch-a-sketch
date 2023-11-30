14bbda751403be0eaa95be418a9217613274a6c7
Churro <27313833+churrou@users.noreply.github.com>
2023-11-30 13:00:40 -0800

    Create initial etch-a-sketch algorithm
    all the functionality is there, but we want
    to add user input to the equation

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const sketchPad = document.querySelector('#sketchPad')
+const sketchPad = document.querySelector('.sketchPad')
 const max = 16;
 
 for (let divCount = 0; divCount < max; divCount++){
     let containers = document.createElement('div');
-    containers.addEventListener('hover', ()=>{
-        
+    containers.addEventListener('mouseover', (event)=>{
+        event.target.id = 'fill'
+        console.log("hovered")
     })
     sketchPad.append(containers)
 }
