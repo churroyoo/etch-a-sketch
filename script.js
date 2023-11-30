e797adda55d9e5ca2d10f53fdcd8e34ac646a8bb
Churro <27313833+churrou@users.noreply.github.com>
2023-11-30 13:52:37 -0800

    Create custom grid size button
    2 new functions. eraseSquares to delete existing board
    and createBoard to create a new board with the
    amount a user has inputted

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const sketchPad = document.querySelector('.sketchPad')
-const max = 16;
+const button = document.querySelector('.setGrid')
 
-for (let divCount = 0; divCount < max; divCount++){
-    let containers = document.createElement('div');
-    containers.addEventListener('mouseover', (event)=>{
-        event.target.id = 'fill'
-        console.log("hovered")
-    })
-    sketchPad.append(containers)
+createBoard(16)
+
+function createBoard(squares) {
+    for (let divCount = 0; divCount < squares; divCount++){
+        let containers = document.createElement('div');
+        containers.addEventListener('mouseover', (event)=>{
+            event.target.id = 'fill'
+            console.log("hovered")
+        })
+        sketchPad.append(containers)
+    }
 }
+function removeSquares(){
+    while (sketchPad.hasChildNodes()){
+        sketchPad.removeChild(sketchPad.firstChild)
+    }
+}
+
+button.addEventListener('click', () => {
+    let squares = prompt('How big?' , 1)
+    removeSquares()
+    createBoard(squares)
+})
